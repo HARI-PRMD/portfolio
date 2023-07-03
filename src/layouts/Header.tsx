cc3d423847e688ba857e1c887e4e70b2fbfc673b
HARI-PRMD <haripramodb@gmail.com>
2023-07-03 17:45:33 +1000

    made header and intro responsive

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,23 +1,55 @@
-const Header = () => {
+import { Transition } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+const Header: React.FC = () => {
+  const [showList, setShowList] = useState(false);
+
   const handleHeadingClick = (sectionId: string) => {
     const section: HTMLElement | null = document.getElementById(sectionId);
     if (section) section.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
-    <div className="fr absolute z-50 h-fit w-full justify-between bg-black/80 px-16 py-4 backdrop-blur-sm">
-      <h2 className="pl-8" onClick={() => handleHeadingClick("main")}>
-        HP
-      </h2>
-      <div className="fr w-full max-w-4xl justify-evenly pt-4">
-        <p className="p2" onClick={() => handleHeadingClick("projects")}>
-          PROJECTS
-        </p>
-        <p onClick={() => handleHeadingClick("volunteering")}>VOLUNTEERING</p>
-        <p>CONTACT</p>
+    <div className="fc absolute z-50 h-fit w-full  bg-black/80 px-4 py-2 backdrop-blur-sm md:px-16 md:py-4">
+      <div className="fr justify-between">
+        <h2 className="h2 md:pl-8" onClick={() => handleHeadingClick("main")}>
+          HP
+        </h2>
+        <div className="md:fr hidden w-full max-w-4xl justify-evenly pt-4">
+          <p onClick={() => handleHeadingClick("projects")}>PROJECTS</p>
+          <p onClick={() => handleHeadingClick("volunteering")}>VOLUNTEERING</p>
+          <p>CONTACT</p>
+        </div>
+        <button className="p hidden h-fit bg-white px-8 py-4 text-black md:block">
+          RESUME
+        </button>
+        <button className="md:hidden" onClick={() => setShowList(!showList)}>
+          <Bars3Icon
+            className={`h-12 w-12 text-white transition-transform duration-300 ${
+              showList ? "-rotate-90" : ""
+            }`}
+          />
+        </button>
       </div>
-      <button className="p h-fit bg-white px-8 py-4 text-black transition-all">
-        RESUME
-      </button>
+      <Transition
+        show={showList}
+        enter="transition duration-300 ease-in-out transform"
+        enterFrom="opacity-0 translate-y-[-20px]"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition duration-200 ease-in transform"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-[-20px]"
+      >
+        <div className="fc w-full space-y-8 pt-8 md:hidden">
+          <p onClick={() => handleHeadingClick("projects")}>PROJECTS</p>
+          <p onClick={() => handleHeadingClick("volunteering")}>VOLUNTEERING</p>
+          <p>CONTACT</p>
+          <button className="p h-fit w-full bg-white px-8 py-4 text-black">
+            RESUME
+          </button>
+        </div>
+      </Transition>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,27 +9,32 @@ export default function Home() {
     <>
       <SEO title="Portfolio" description="Landing Page" />
       <Layout>
-        <section id="main" className="fr min-h-screen w-full space-x-8 p-32">
-          <div className="fc h-full w-3/5 justify-center">
-            <div className="space-y-12">
-              <h1>Hari Pramod</h1>
-              <p className="p-main">
+        <section
+          id="main"
+          className="md:fr flex h-screen w-full flex-col-reverse p-8 md:space-x-8 md:p-32"
+        >
+          <div className="fc bg-rd h-full items-start justify-start pt-8 md:w-3/5 md:justify-center">
+            <div className="fc space-y-4 md:space-y-12">
+              <h1 className="h3 md:h1">Hari Pramod</h1>
+              <p className="subtitle md:p-main ">
                 I am a software engineering student excited about the
                 opportunity to apply my skills and knowledge to real-world
                 software product design and development.
               </p>
-              <button className="p-main w-fit border-2 px-16 py-4">
+              <button className="subtitle md:p-main w-full border-2 px-4 py-2 md:w-fit md:px-16 md:py-4">
                 See Resume
               </button>
             </div>
           </div>
-          <Image
-            src="https://media.discordapp.net/attachments/1092987636035092662/1125071586228187166/image0.jpg"
-            width={919}
-            height={1225}
-            className="h-full w-2/5 object-cover brightness-110 contrast-125 grayscale"
-            alt="portrait"
-          />
+          <div className="fc h-full justify-end md:w-2/5 md:items-center md:justify-center">
+            <Image
+              src="https://media.discordapp.net/attachments/1092987636035092662/1125071586228187166/image0.jpg"
+              width={919}
+              height={1225}
+              className="h-64 max-h-fit w-full object-cover brightness-110 contrast-125 grayscale md:h-4/5"
+              alt="portrait"
+            />
+          </div>
         </section>
         <section
           id="projects"
