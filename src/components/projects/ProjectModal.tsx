11f901183b912d2a15cbc5b623b693aba9cefbf6
HARI-PRMD <haripramodb@gmail.com>
2023-07-21 23:57:49 +1000

    updated modal styling and layout

--- src/components/projects/ProjectModal.tsx
+++ src/components/projects/ProjectModal.tsx
@@ -23,21 +23,21 @@ const ProjectModal: React.FC<Props> = ({
   return (
     <div
       id="background"
-      className="fr fixed left-0 top-0 z-50 h-screen w-screen items-center justify-center bg-black/50"
+      className="fr fixed left-0 top-0 z-50 h-screen w-screen items-center justify-center bg-black/50 backdrop-blur-sm"
       onClick={(e) => onClose(e)}
     >
       <div
-        className="fc scroller container h-full overflow-y-auto border bg-black md:h-5/6"
+        className="fc scroller container h-full overflow-y-auto border border-white/20 bg-black md:h-5/6"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="fr sticky top-0 w-full justify-between border-b border-r bg-black px-8 py-4">
+        <div className="fr sticky top-0 w-full justify-between border-white/20 bg-black/90 px-8 py-4 backdrop-blur">
           <h3>{title}</h3>
           <XMarkIcon
             onClick={onClose}
             className="h-8 w-8 cursor-pointer text-white md:h-12 md:w-12"
           />
         </div>
-        <div className="w-full space-y-8 p-4 md:p-24">
+        <div className="w-full space-y-4 p-4 md:p-24">
           <div className="fc w-full items-center justify-center">
             {image ? (
               <Image
@@ -45,20 +45,20 @@ const ProjectModal: React.FC<Props> = ({
                 width={2151}
                 height={1227}
                 alt="megalan"
-                className="h-fit w-full max-w-lg object-cover"
+                className="h-fit w-full object-cover md:w-4/5"
               />
             ) : (
-              <div className="hero-pattern h-48 max-w-lg object-cover"></div>
+              <div className="hero-pattern h-48 w-full object-cover md:w-4/5"></div>
             )}
+            <p className="subtitle w-full pt-2 text-left opacity-60 md:w-4/5">
+              {year ? month + " " + year.toString() : month}
+            </p>
           </div>
-          <p className="subtitle">
-            {year ? month + " " + year.toString() : month}
-          </p>
           <MarkdownParser markdownString={description} />
           <div className="fc md:fr w-full space-y-4 md:space-x-8 md:space-y-0">
             {websiteLink && (
               <Link href={websiteLink}>
-                <div className="fr w-full items-center justify-center space-x-4 border-2 px-8 py-2 md:w-fit">
+                <div className="fr w-full items-center justify-center space-x-4 border border-white/20 bg-white/5 px-8 py-2 transition-colors duration-300 hover:border-white/40 hover:bg-white/10 md:w-fit">
                   <p>Website</p>
                   <ArrowTopRightOnSquareIcon className="h-6 w-6 text-white" />
                 </div>
@@ -66,7 +66,7 @@ const ProjectModal: React.FC<Props> = ({
             )}
             {codeLink && (
               <Link href={codeLink}>
-                <div className="fr w-full items-center justify-center space-x-4 border-2 px-8 py-2 md:w-fit">
+                <div className="fr w-full items-center justify-center space-x-4 border border-white/20 bg-white/5 px-8 py-2 transition-colors duration-300 hover:border-white/40 hover:bg-white/10 md:w-fit">
                   <p>Code</p>
                   <CodeBracketIcon className="h-6 w-6 text-white" />
                 </div>
