2efa05463a8830ccd6633c3b20c5b839e6107aca
HARI-PRMD <haripramodb@gmail.com>
2023-07-22 00:00:09 +1000

    minor alignment adjustment for volunteering cards

--- src/components/volunteering/VolunteerCard.tsx
+++ src/components/volunteering/VolunteerCard.tsx
@@ -12,7 +12,7 @@ const VolunteerCard: React.FC<Props> = ({
   description,
 }) => {
   return (
-    <div className="fc h-full w-full min-w-[16rem] snap-x snap-center items-center justify-center border border-white/20 bg-white/5 p-8 grayscale transition-all duration-300 hover:grayscale-0 md:h-fit">
+    <div className="fc h-full w-full min-w-[16rem] snap-x snap-center items-center justify-center border border-white/20 bg-white/5 p-8 grayscale transition-all duration-300 hover:grayscale-0 md:h-fit md:items-start md:justify-start">
       <div className="fc md:fr items-center md:space-x-8">
         <Image
           src={image}
