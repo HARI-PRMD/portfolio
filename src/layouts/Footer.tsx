47b23472fd928700f43702ebbfcb48e43169b447
HARI-PRMD <haripramodb@gmail.com>
2025-11-16 19:35:12 +1100

    Tweak footer styling

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,14 +2,17 @@ const Footer = () => {
   // TODO: Implement hero wave pattern at bottom left of website
   // <div className="hero-pattern absolute z-40"></div>
   return (
-    <div id="footer" className="h-fit w-full p-8 text-2xl">
-      <p className="opacity-60">
+    <div
+      id="footer"
+      className="h-fit w-full px-6 py-8 text-xs text-neutral-400 sm:px-8 sm:text-sm md:text-base"
+    >
+      <p className="text-center tracking-wide">
         Designed and Developed by{" "}
         <a
           href="https://github.com/HARI-PRMD"
           target="_blank"
           rel="noreferrer"
-          className="font-semibold underline-offset-4 hover:underline"
+          className="font-semibold text-white underline-offset-4 hover:underline"
         >
           @HARI-PRMD (Me!)
         </a>
